7a1eeebd5f5f9a329f0967d40a6205771fbad5a1
Romain DESMIDT <romain.desmidt@gmail.com>
2019-07-01 18:23:41 +0200

    app back end skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'annonces#index'
+  resources :annonces
   get 'social', to: 'pages#social'
-  get 'annonces', to: 'annonces#index'
-  get 'annonces/new', to: 'annonces#new'
-  post 'annonces', to: 'annonces#create'
+  # get 'annonces', to: 'annonces#index'
+  # get 'annonces/new', to: 'annonces#new'
+  # post 'annonces', to: 'annonces#create'
+  get  'searchannonce', to: 'annonces#search'
+  # get 'annonces/:id', to: 'annonces#show'
+  get 'users/me', to: 'users#me'
+  get 'users/:id', to: 'users#show'
+  get 'users', to: 'users#index'
+  # get "annonces/:id/edit", to: "annonces#edit"
+  # patch "annonces/:id", to: "annonces#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
