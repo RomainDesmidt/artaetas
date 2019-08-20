c6eca2247321291542d3020fb19099a08ea4b540
Romain DESMIDT <romain.desmidt@gmail.com>
2019-08-20 18:43:48 +0200

    follow +like user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,15 @@ class User < ApplicationRecord
   has_many :courants, through: :courant_users
   has_many :orders
 
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followees, through: :followed_users
+
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  has_many :followers, through: :following_users
+
+
   mount_uploader :photoprofil, PhotoUploader
   mount_uploader :photofond, PhotoUploader
   acts_as_voter
+  acts_as_votable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_091018) do
+ActiveRecord::Schema.define(version: 2019_08_19_075852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,13 @@ ActiveRecord::Schema.define(version: 2019_08_16_091018) do
   create_table "courants", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
