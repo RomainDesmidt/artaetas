e599b92004580c3170c9b54ee1870a0385258333
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-09-30 18:38:24 +0000

    query select ex2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,11 +6,19 @@
 // 	document.getElementById("ex6SliderVal").textContent = sliderValue;
 // });
 
-var slider = new Slider('#ex2', {
-  formatter: function(value) {
-		return 'Prix: ' + value;
-	},
-});
+
+
+var slidons = document.querySelector('#ex2') !== null;
+if (slidons) {
+  var slider = new Slider('#ex2', {
+    formatter: function(value) {
+  		return 'Prix: ' + value;
+  	},
+  });
+
+}
+
+
 
 var selection = document.querySelector('.multiple-dd') !== null;
 if (selection) {
