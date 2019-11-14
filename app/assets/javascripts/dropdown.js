2189aafb0d79fd1bfe63323d768c0d71b53df563
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-14 18:29:40 +0000

    corrected old model association error, params require permit awkwardness and active admin awesomeness

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,1 +1,38 @@
+//= require slimselect.min
+var selection = document.querySelector('.multiple-dd') !== null;
+if (selection) {
+   var select = new SlimSelect({
+  select: document.querySelector('.multiple-dd')
+  
+})
 
+if ( document.querySelector('div.multiple-dd>div.ss-multi-selected>div.ss-values>span.ss-disabled') != null )  {
+  document.querySelector('div.multiple-dd>div.ss-multi-selected>div.ss-values>span.ss-disabled').innerText = "Choisissez une catégorie...";
+}
+  document.querySelector('div.multiple-dd>div.ss-content>div.ss-search>input').placeholder = "Saisie";
+
+};
+
+var selection2 = document.querySelector('.multiple-dd2') !== null;
+if (selection2) {
+   var select2 = new SlimSelect({
+  select: document.querySelector('.multiple-dd2')
+  
+})
+if ( document.querySelector('div.multiple-dd2>div.ss-multi-selected>div.ss-values>span.ss-disabled') != null )  {
+  document.querySelector('div.multiple-dd2>div.ss-multi-selected>div.ss-values>span.ss-disabled').innerText = "Choisissez un courant...";
+}
+  document.querySelector('div.multiple-dd2>div.ss-content>div.ss-search>input').placeholder = "Saisie";
+};
+
+var selection3 = document.querySelector('.multiple-dd3') !== null;
+if (selection3) {
+   var select3 = new SlimSelect({
+  select: document.querySelector('.multiple-dd3')
+  
+})
+if ( document.querySelector('div.multiple-dd3>div.ss-multi-selected>div.ss-values>span.ss-disabled') != null )  {
+  document.querySelector('div.multiple-dd3>div.ss-multi-selected>div.ss-values>span.ss-disabled').innerText = "Choisissez une couleur...";
+}
+  document.querySelector('div.multiple-dd3>div.ss-content>div.ss-search>input').placeholder = "Saisie";
+};
--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,8 +1,8 @@
 class Annonce < ApplicationRecord
   
-  validates :photo, :name, :description,  presence: true
-  validates :prix, format: {with: /\A\d+(?:\.\d{0,2})?\z/}, numericality: {less_than: 1000000}, presence: true
-  validates :anneecreation, length: {maximum: 4}
+ validates :photo, :name, :description,  presence: true
+ validates :prix, format: {with: /\A\d+(?:\.\d{0,2})?\z/}, numericality: {less_than_or_equal_to: 1000000}, presence: true
+ validates :anneecreation, length: {maximum: 4}
   acts_as_votable
   belongs_to :user
   
@@ -23,7 +23,7 @@ class Annonce < ApplicationRecord
 
   acts_as_votable
   act_as_bookmarkee
-
+  accepts_nested_attributes_for :categorie_annonces, :courant_annonces, :couleur_annonces
 
   # def couleur_ids=(ids)
   # end
@@ -34,7 +34,7 @@ class Annonce < ApplicationRecord
   # def categorie_ids=(ids)
   # end
   
-  def cat_ids=(ids)
-  end
+ # def cat_ids=(ids)
+ # end
   
 end
