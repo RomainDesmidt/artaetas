dc622f7b8b037f11a4a61d8a2f0f8e9cf57f11b5
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-15 15:08:57 +0000

    add admin user

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :username, :login, :email, :password, :password_confirmation
 
   index do
     selectable_column
@@ -17,7 +17,8 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs do
-      f.input :login
+      f.input :username
+      f.input :email
       f.input :password
       f.input :password_confirmation
     end
