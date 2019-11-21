01096ea804a239f82892e98e582d0468aef740f0
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-21 17:16:38 +0000

    photo error

--- app/admin/annonces.rb
+++ app/admin/annonces.rb
@@ -19,12 +19,12 @@ ActiveAdmin.register Annonce do
             row "Photo 1" do |image|
                 image_tag image.photo.url, style: "width: 50px; height: 50px;"
             end
-            row "Photo 2" do |image|
-                image_tag image.photo_un.url, style: "width: 50px; height: 50px;"
-            end
-            row "Photo 3" do |image|
-                image_tag image.photo_deux.url, style: "width: 50px; height: 50px;"
-            end
+            # row "Photo 2" do |image|
+            #     image_tag image.photo_un.url, style: "width: 50px; height: 50px;"
+            # end
+            # row "Photo 3" do |image|
+            #     image_tag image.photo_deux.url, style: "width: 50px; height: 50px;"
+            # end
             row :name
             row :description
             row :prix
