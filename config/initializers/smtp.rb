6963a7acd65588e5c30d14ef3bd650e3738b7d5d
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-25 10:30:35 +0000

    test ovh

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,18 @@
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.sendgrid.net',
+#   port: 587,
+#   domain: 'http://artaetasv1.herokuapp.com',
+#   user_name: ENV['SENDGRID_USERNAME'],
+#   password: ENV['SENDGRID_PASSWORD'],
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
+  address: 'ssl0.ovh.net',
+  port: 465,
   domain: 'http://artaetasv1.herokuapp.com',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['OVH_USERNAME'],
+  password: ENV['OVH_MAIL_PASSWORD'],
   authentication: :login,
   enable_starttls_auto: true
 }
