a1f2d042d961c692f184c67d9715583a0c01d1d0
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-25 19:00:16 +0000

    artaetas ovh pro

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -10,7 +10,8 @@
 ActionMailer::Base.smtp_settings = {
   address: 'pro1.mail.ovh.net',
   port: 587,
-  domain: 'http://artaetasv1.herokuapp.com',
+#   domain: 'http://artaetasv1.herokuapp.com',
+  domain: 'http://www.artaetas.fr',
   user_name: ENV['OVH_USERNAME'],
   password: ENV['OVH_MAIL_PASSWORD'],
   authentication: :login,
