50617d2d6161b6f63e4d88d905c88ac22b29145d
EC2 Default User <ec2-user@ip-172-31-5-11.eu-west-1.compute.internal>
2019-11-29 10:57:39 +0000

    mailer confirmation + welcome

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def welcome
     @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: 'Welcome to Artaetas')
+    mail(to: @user.email, subject: 'Votre compte membre ARTAETAS est créé !')
     # This will render a view in `app/views/user_mailer`!
   end
 end
