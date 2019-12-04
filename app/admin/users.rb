21e23142fbabc13d5aac884eba10b5a371911f80
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2019-12-04 18:56:50 +0000

    approuve

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -14,7 +14,7 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
-    column "Censuré?", :confirmation_webmaster
+    column "Approuvé?", :confirmation_webmaster
     column :email
     column :username
     column :created_at
