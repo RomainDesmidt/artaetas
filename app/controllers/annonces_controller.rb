1c2d89820a5323d6a6e57754c326d927a287d1b0
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2019-12-17 01:51:27 +0000

    randomize order

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -18,8 +18,8 @@ class AnnoncesController < ApplicationController
     @annonces_confirmeduser = Annonce.joins(:user).where("users.confirmation_webmaster = true")
     @annonces_all = @annonces_confirmeduser.where(envente_yesno: true)
     @landingp = 1
-    @annonces_premium = @annonces_all.where(formule: "Mise en Avant").or(@annonces_all.where(formule: "Mise a la une"))
-    @annonces_standard = @annonces_all.where(formule: "Standard")
+    @annonces_premium = @annonces_all.where(formule: "Mise en Avant").or(@annonces_all.where(formule: "Mise a la une")).order('random()')
+    @annonces_standard = @annonces_all.where(formule: "Standard").order('random()')
     @annonces = @annonces_standard.to_a
     @annonces_pre = @annonces_premium.to_a
     
