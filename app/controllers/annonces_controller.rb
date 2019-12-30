4a872b1576e3b198e141a8134c195432a38c2836
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2019-12-30 15:53:00 +0000

    typo code postal

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -127,7 +127,7 @@ class AnnoncesController < ApplicationController
     @volume = params[:volume]
     # @code_postal_all = User.all.collect { |x| x.codepostal.divmod(1000)[0]}
     @code_postal_all = User.all.collect do |x| 
-      unless x.code_postal.nil?
+      unless x.codepostal.nil?
         x.codepostal.divmod(1000)[0]
       end
     end
