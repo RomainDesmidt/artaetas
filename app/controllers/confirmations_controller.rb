f3532a0acc5943071b8c113bd66c48be41d747d2
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-01-03 18:00:58 +0000

    notice after confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
-    redirect_to root_path
+    redirect_to root_path, flash: { success: "Nous vous invitons à compléter votre profil !" }
+    # redirect_to users_me_path( :view_param => "annonce")
   end
 end
