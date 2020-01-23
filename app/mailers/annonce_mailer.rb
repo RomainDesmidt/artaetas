d976f824a2524fbccd15c71f60db8cb3045b86bc
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-01-23 12:16:40 +0000

    mailing test

--- app/mailers/annonce_mailer.rb
+++ app/mailers/annonce_mailer.rb
@@ -18,7 +18,7 @@ class AnnonceMailer < ApplicationMailer
     @intercedant = params[:intercedant]
     @corps = params[:corps]
     @sujet = params[:sujet]
-    mail(to: @user_annonce.email, subject: @sujet)
+    mail(to: @user_annonce.email, subject: "l'utilisateur #{@intercedant}, vous envoie un message suite à votre annonce")
   end
 
 end
