d682207192541f204bd42b7a88b90f35e928a11e
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-03-03 19:32:53 +0000

    domain name + slug tweak

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-  domain: 'http://artaetasv1.herokuapp.com',
+  domain: 'http://www.artaetas.com',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   authentication: :login,
