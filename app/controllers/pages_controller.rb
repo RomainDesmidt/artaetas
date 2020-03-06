e9fca30aa1285f9d8f3038b6427a4fd4825b99fc
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-03-06 11:16:02 +0000

    mobile mieux

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home,:social, :cgu, :politiquedeconfidentialite, :mentionslegales]
+  skip_before_action :authenticate_user!, only: [:home,:social, :cgu, :politiquedeconfidentialite, :mentionslegales, :quisommesnous, :faq]
 
   def home
   end
