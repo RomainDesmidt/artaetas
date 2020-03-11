74a062e635eca084a906eb6654c8cbc1f513d2dd
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-03-11 15:55:46 +0000

    espace

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -4,7 +4,7 @@ var slidons = document.querySelector('#ex2') !== null;
 if (slidons) {
   var slider = new Slider('#ex2', {
     formatter: function(value) {
-  		return 'Min: ' + value[0] + '€'  + '- Max: ' + value[1] + '€';
+  		return 'Min: ' + value[0] + '€'  + ' - Max: ' + value[1] + '€';
   		// return 'Prix: ' +  value;
   	},
   });
