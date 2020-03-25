b4c81a92c3d362e7dee23fc8265ce172a4cad996
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-03-25 08:51:01 +0000

    photo.nil?

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -18,6 +18,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
   
    def low
+     unless self.nil?
       httpsimagepath = self.url.insert(4, 's')
       # httpsimagepath.slice! self.filename
       filename = self.url.split('/').last
@@ -30,21 +31,24 @@ class PhotoUploader < CarrierWave::Uploader::Base
         httpsimagepath = httpsimagepath + 'q_auto:low/' + filename
       end
       return httpsimagepath
+    end
    end
    
     def exifignore
-      httpsimagepath = self.url.insert(4, 's')
-      # httpsimagepath.slice! self.filename
-      filename = self.url.split('/').last
-      version = self.url.split('/').last(2).first
-      httpsimagepath.slice! filename
-      if version.match('[v]\d*').to_s == version
-        httpsimagepath.slice! version + '/'
-        httpsimagepath = httpsimagepath + 'a_ignore/' + version + '/' + filename
-      else
-        httpsimagepath = httpsimagepath + 'a_ignore/' + filename
-      end
-      return httpsimagepath
+      unless self.nil?
+        httpsimagepath = self.url.insert(4, 's')
+        # httpsimagepath.slice! self.filename
+        filename = self.url.split('/').last
+        version = self.url.split('/').last(2).first
+        httpsimagepath.slice! filename
+        if version.match('[v]\d*').to_s == version
+          httpsimagepath.slice! version + '/'
+          httpsimagepath = httpsimagepath + 'a_ignore/' + version + '/' + filename
+        else
+          httpsimagepath = httpsimagepath + 'a_ignore/' + filename
+        end
+        return httpsimagepath
+      end 
    end
    
    
