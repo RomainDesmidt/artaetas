01aa6f54d408b5bda9186bcbb7f3c109bb31fa3f
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-04-13 16:29:17 +0000

    back office order unitaire

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -12,4 +12,34 @@ ActiveAdmin.register Order do
 #   permitted
 # end
 
+  action_item :touchjour, only: [:show], if: proc { Order.find(params[:id]).ongoing_subscription == true }  do 
+    link_to 'RaZ', renewday_admin_order_path, method: :put
+  end
+  
+  member_action :renewday, :method => :put do
+    @modif_order = Order.find(params[:id])
+    @modif_order.touch
+    redirect_to admin_order_path
+  end
+
+  action_item :extunjour, only: [:show], if: proc { Order.find(params[:id]).ongoing_subscription == true }  do 
+    link_to 'Jour+', onemoreday_admin_order_path, method: :put
+  end
+  
+  member_action :onemoreday, :method => :put do
+    @modif_order = Order.find(params[:id])
+    @modif_order.update(updated_at: @modif_order.updated_at + 1.day )
+    redirect_to admin_order_path
+  end
+  
+ action_item :remunjour, only: [:show], if: proc { Order.find(params[:id]).ongoing_subscription == true }  do 
+    link_to 'Jour-', onelessday_admin_order_path, method: :put
+  end
+  
+  member_action :onelessday, :method => :put do
+    @modif_order = Order.find(params[:id])
+    @modif_order.update(updated_at: @modif_order.updated_at - 1.day )
+    redirect_to admin_order_path
+  end
+
 end
