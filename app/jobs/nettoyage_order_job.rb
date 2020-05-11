45faf5bb854c9b7f52caa70605a0552c928a966c
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-05-11 15:29:29 +0000

    nettoyage order desc + check on checkout session id

--- app/jobs/nettoyage_order_job.rb
+++ app/jobs/nettoyage_order_job.rb
@@ -6,18 +6,20 @@ class NettoyageOrderJob < ApplicationJob
     require 'stripe'
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     ActiveRecord::Base.connection_pool.with_connection do
-      Order.where(state: "pending").each do |order|
+      Order.where(state: "pending").order(updated_at: :desc).each do |order|
         puts "l'id de la session est : #{order.checkout_session_id}"
         puts "l'id de l'order est : #{order.id}"
-        query_checkout = Stripe::Checkout::Session.retrieve(
-          order.checkout_session_id,
-        )
-        sleep 2
-        query_payment = Stripe::PaymentIntent.retrieve(
-          query_checkout["payment_intent"],
-        )
-        puts query_payment["status"]
-        sleep 2
+        unless order.checkout_session_id.nil?  
+          query_checkout = Stripe::Checkout::Session.retrieve(
+            order.checkout_session_id,
+          )
+          sleep 2
+          query_payment = Stripe::PaymentIntent.retrieve(
+            query_checkout["payment_intent"],
+          )
+          puts query_payment["status"]
+          sleep 2
+        end
       end
     end
   end
