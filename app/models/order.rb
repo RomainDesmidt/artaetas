3f66260b955ea0362b9b577540a0cf8f80dfe558
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-05-11 13:50:27 +0000

    execute later order.id

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   before_create :set_slug
+  after_create :execute_later
   belongs_to :user
   belongs_to :annonce
   monetize :amount_cents
@@ -10,5 +11,10 @@ class Order < ApplicationRecord
      self.slug = SecureRandom.uuid
      break unless Order.where(slug: slug).exists?
     end
+  end
+  
+  def execute_later
+    puts "l'id de cette order est : #{self.id}"
+    CallingOrderJob.perform_in(10)
   end
 end
