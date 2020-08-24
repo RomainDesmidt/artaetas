4a7bab70adc966846f22257b4c8f29e4fc008a76
Wagonnet de la Loire <wagonnetdelaloire@gmail.com>
2020-08-24 16:03:46 +0000

    contribution webhook

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    if order.premium_sku == "Don"
+    if order.premium_sku == "Don" || order.premium_sku == "Contribution"
       order.update(state: 'paid')
     else
       order.update(state: 'paid', ongoing_subscription: true)
